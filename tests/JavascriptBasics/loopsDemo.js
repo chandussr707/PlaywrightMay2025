c3860e36743e763a952b582cae0e34365821b5ea
chandussr707 <chandussr@gmail.com>
2025-07-16 11:45:08 +0530

    first commit

---
+++ tests/JavascriptBasics/loopsDemo.js
@@ -0,0 +1,23 @@
+
+// let programmingLanguage = ["Satsh", "Chandra", "Sareddy", 4];
+
+// for(let p1 of programmingLanguage){
+//     console.log(p1)
+// }
+// let dateOfPerson = {
+//     name: "Ram",
+//     Age: 20,
+//     Department: "TT",
+//     isAvailable: true
+// }
+// for(let keyDemo in dateOfPerson){
+//     console.log(dateOfPerson[keyDemo])
+// }
+let str = " I am new to Playwright with JavaScript";
+let news = str.split("").reverse().join("");
+let news1 = str.split("").reverse().join("").split(" ").reverse().join(" ");
+let news3 = str.split(" ").reverse().join(" ");
+
+console.log(news)
+console.log(news1)
+console.log(news3)
---
+++ tests/JavascriptBasics/variableDemo.js
@@ -0,0 +1,20 @@
+function varDemo()
+{
+ var m = 200;
+ console.log(m)
+ var str = "Satish"
+
+ if(true)
+ {
+    console.log(m)
+    var m = 400;
+     var bool = true
+    console.log(m)
+ }
+ console.log(m)
+ console.log(bool)
+
+}
+var bool = false;
+varDemo();
+console.log(bool)
